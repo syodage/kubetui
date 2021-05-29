175afbca97f74ff2ae02ecaad38f6bd089728086
Shameera Rathnayaka Yodage <shameerayodage@gmail.com>
2021-05-28 22:27:44 -0400

    Set fixed size for logview to show one line of log

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ func main() {
 
 	k := NewKubetui(app)
 	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(k.contextView, 0, 2, false).
+		AddItem(k.contextView, 0, 3, false).
 		AddItem(tview.NewFlex().
 			AddItem(k.menu, 0, 1, false).
 			AddItem(k.mainView, 0, 7, false),
-			0, 8, false).
-		AddItem(k.logView, 0, 1, false)
+			0, 18, false).
+			// set fixed size 3, which shows only one line, set propotion to 1 otherwise.
+		AddItem(k.logView, 3, 0, false)
 
 	if err := app.SetRoot(flex, true).SetFocus(k.menu).Run(); err != nil {
 		panic(err)
